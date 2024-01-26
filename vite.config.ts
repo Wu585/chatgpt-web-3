f9f059556a32a405b24bac5c61d2677e4f5f1e45
wu649782027 <15151851516@163.com>
2024-01-26 10:15:41 +0800

    change

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
-import path from "path"
+import * as path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,7 +13,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000/api/v1',
+        target: 'http://localhost:8000/api/v1',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
